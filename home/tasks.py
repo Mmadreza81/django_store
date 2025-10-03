7114d0a6c3e14362ed933550eede59f2c8813783
Mmadreza81 <mohammadrezashokri084@gmail.com>
2025-10-03 21:01:44 +0330

    project has been done

--- home/tasks.py
+++ home/tasks.py
@@ -1,7 +1,7 @@
 from bucket import bucket
 from A.celery_conf import celery_app
 
-# TODO:can be async?
+# can be async?
 def all_bucket_objects_task():
     result = bucket.get_objects()
     return result
